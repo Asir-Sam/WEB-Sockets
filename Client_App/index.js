c8f23e44c4c6060316759e1af36fb69710422e7f
sam <asirsam@rishoncomm.in>
2024-02-07 21:57:33 +0530

    Visualizing Client HTML added , CORS header added

--- Client_App/index.js
+++ Client_App/index.js
@@ -1,5 +1,17 @@
-import { io } from 'socket.io-client';
-
+import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
 const socket = io('http://localhost:4000');
+const submitButton = document.getElementById('send');
+const inputMessageField = document.getElementById('message');
+const userNameField = document.getElementById('username');
 
-socket.emit('message', 'Hi');
+  submitButton.onclick = () =>{
+
+        const inputMessage =  inputMessageField.value;
+        socket.emit("message", inputMessage, (response) => {
+
+           console.log('Server Response',response); 
+           userNameField.innerText = response;
+      
+         });
+
+  }
--- Server_App/index.js
+++ Server_App/index.js
@@ -1,13 +1,18 @@
-const socket = require('socket.io')(4000);
+const socket = require('socket.io')(4000,{
+
+        cors :{ orgin:["https://localhost:5500","https://192.168.1.7:5500"] }
+
+});
 
 socket.on('connection', soc => {
 
     console.log('socket ID', soc.id);
 
-    soc.on('message', (message) => {
+    soc.on('message', (message, callback) => {
 
         console.log(message);
-
-    })
+        callback('Received message from server'+message);
+        
+    });
 
 });
